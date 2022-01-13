834ca62641ea3b4f4a6fd0612f88bd1e8e5951a9
Sumit Lahiri <lahiri.phdworks@gmail.com>
2022-01-14 01:12:03 +0530

    Slides & Updates

--- contract-samples/contracts/Test.sol
+++ contract-samples/contracts/Test.sol
@@ -1,32 +1,52 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.4.2 <0.9.0;
+// Not so Smart Contract.
 
 contract Test {
     uint256 counter = 0;
     address public owner;
     uint256 contractBalance = 0;
+    // Basic Event Lgging
+    event Log(address indexed sender, string message);
+    event Looper(string message, uint256 number);
+    
+    
+    // Function to receive Ether. msg.data must be empty
+    receive() external payable {}
+
+    // Fallback function is called when msg.data is not empty
+    fallback() external payable {}
 
     constructor() public {
         owner = msg.sender;
     }
 
     function depositOwner() public payable {
-        payable(owner).transfer(msg.value);
+         (bool sent, bytes memory data) = payable(owner).call{value:msg.value}("");
+         emit Log(msg.sender, "Deposit Ether to Owner");
+         require(sent, "Failed to send Ether");
     }
 
+    function depositContract() public payable {
+        (bool sent, bytes memory data) = payable(address(this))
+                                            .call{value:msg.value}("");
+        emit Log(msg.sender, "Deposit Ether to Contract");     
+        require(sent, "Failed to send Ether");
+    }
+    
     function testAdhoc(uint256 a, uint256 b) public pure returns (uint256) {
         return (a + b) * (a - b);
     }
 
     // This gives the amount/ether strored in the smart contract.
-    function getBalance() public view returns (uint256 bal) {
+    function getContractBalance() public view returns (uint256 bal) {
         return address(this).balance;
     }
 
     // This gives the balance of the owner account.
     // Not the amount/ether strored in the smart contract.
     // The name is confusing here.
-    function getContractBalance() public view returns (uint256 bal) {
+    function getOwnerBalance() public view returns (uint256 bal) {
         return owner.balance;
     }
 
@@ -34,7 +54,18 @@ contract Test {
     function getBalanceSender() public view returns (uint256 bal) {
         return msg.sender.balance;
     }
-
+    
+    // web3.utils.toWei('1', 'ether') else goes to fallback since
+    // arguments error.
+    function transferEther(address payable addressToSend, uint256 amount)
+        public
+        payable
+    {
+        (bool sent, bytes memory data) = addressToSend
+                           .call{value:amount}("");
+        require(sent, "Failed to send Ether");
+    }
+    
     function transferToAddressStub(
         uint256 a,
         uint256 b,
@@ -45,20 +76,16 @@ contract Test {
         if (counter != a) {
             // count may go negative : revert.
             while (count != 0) {
-                // reverts if msg.value is lower than 1 ether here.
-                // Ether is lost.
-                addressToSend.transfer(1 ether);
+                // reverts if msg.value is lower than required 
+                // ether here.
+                // Ether is lost to contract here if extra 
+                // ether sent >= amout to transfer.
+                (bool sent, bytes memory data) = addressToSend
+                                   .call{value:0.5 ether}("");
+                emit Looper("Looping", count);
+                require(sent, "Failed to send Ether");
                 count = count - 1;
             }
         }
-    }
-
-    // web3.utils.toWei('1', 'ether') else goes to fallback since
-    // arguments error.
-    function transferEther(address payable addressToSend, uint256 amount)
-        public
-        payable
-    {
-        addressToSend.transfer(amount);
     }
 }
--- contract-samples/test.js
+++ contract-samples/test.js
@@ -1,6 +1,6 @@
-const Test = artifacts.require("Test");
+const Test = artifacts.require("Test.sol");
 
-let Main =  async () => {   
+let Main =  async (callback) => {   
    
     let TestHook = async () => {
         const sampleInstance = await Test.deployed();
@@ -17,54 +17,139 @@ let Main =  async () => {
         // same. Say accounts[0] was used to create, use a different account
         // accounts[1] call this function. This will deposit Ether 
         // to account creator's contract address.
-        var result = await sampleInstance.depositOwner({
-                from: accounts[1], 
-                // Value can't be zero or empty for this 
-                // call to pass through.
-                value: web3.utils.toWei('1', 'ether')
-            });
-        console.log(result)
+        // COMMENT :
+        // Owner is address[0] if that is used to 
+        // create the contract.
+        // var result = await sampleInstance.depositOwner({
+        //         from: accounts[1], 
+        //         // Value can't be zero or empty for this 
+        //         // call to pass through.
+        //         value: web3.utils.toWei('0.08', 'ether')
+        //     });
+            
+        // console.log(result)
         
         // web3.utils.toWei('1', 'ether') else goes to fallback since
         // arguments error.
-        // Correct is to use web3.utils.toWei('1', 'ether')
+        // COMMENT :
+        // Correct is to use web3.utils.toWei('x', 'ether')
         // Transfer's from contract's funds/address "amount" ether if
         // from is not supplied, else address in "from" used as source 
         // to transfer ether to "arg1", address. 
-        var result = await sampleInstance.transferEther(
-            accounts[0],
-            // amount <= msg.value else SM reverts.
-            web3.utils.toWei('2', 'ether'), 
-            {
-                from: accounts[1], 
-                // Value can't be zero or empty for this 
-                // call to pass through.
-                gas: 1000000,
-                value: web3.utils.toWei('2', 'ether')
-            }
-        );
+        // ASSERT : Ether is deducted from contract if from is unspecified, 
+        // else revert/error occurs given contract has less ether than 
+        // amount to be sent.
+        // var result = await sampleInstance.transferEther(
+        //     accounts[1],
+        //     web3.utils.toWei('1', 'ether'), 
+        //     // {
+        //     //     from: accounts[1], 
+        //     //     // Value can't be zero or empty for this 
+        //     //     // call to pass through.
+        //     //     gas: 1000000,
+        //     //     value: web3.utils.toWei('2', 'ether')
+        //     // }
+        // );
         
+        // COMMENT :
         // From account to args2 address account.
         // transfer : b * (1 ether) if msg.value >= b * (1 ether)
+        // [23:48:23]  Transaction: 0xf6549acc5e359400e1068c3429947139a7723f8e23a72e6ea8ab1dc6256548c2
+        // [23:48:24]  Gas usage: 6721974
+        // [23:48:24]  Block Number: 40
+        // [23:48:24]  Block Time: Thu Jan 13 2022 23:48:23 GMT+0530 (India Standard Time)
+        // [23:48:24]  Runtime Error: out of gas
+        // Extra ether sent with the transaction is now with 
+        // contract.
+        
+        // COMMENT : Get base gas level. (Threshold).
+        Test.web3.eth.getGasPrice((error, result) => { 
+            var gasPrice = Number(result);
+            console.log("Gas Price is " + gasPrice + " wei"); // "10000000000000"
+            // Get Contract instance
+            Test.deployed().then((instance) => {
+                // Use the keyword 'estimateGas' after the function name to get 
+                // the gas estimation for this particular function 
+                return instance.transferToAddressStub.estimateGas(
+                    100, // args0
+                    800, // args1
+                    accounts[0],
+                    {
+                        from: accounts[1], 
+                        // Value can't be zero or empty for this 
+                        // call to pass through.
+                        value: web3.utils.toWei('400', 'ether') // msg.value
+                    }
+                )
+            }).then((result) => {
+                // COMMENT : Print Threshold.
+                // ASSERT Constraint-5 : Threshold == gas
+                var gas = Number(result);
+                console.log("gas estimation = " + gas + " units");
+                console.log("gas cost estimation = " + (gas * gasPrice) + " wei");
+                console.log("gas cost estimation = " + Test.web3.utils.fromWei(
+                    web3.utils.toBN(gas * gasPrice), "ether") + " ether"
+                );
+            });
+        });
+        
+        // ASSERT Internal Constant : ether_weight = 0.5 ether
+        var contract_balance = await sampleInstance.getContractBalance();
+        console.log(contract_balance.toString())
+        // ASSERT Constraint-1 : contract_balance + msg.value == args1 * ether_weight. 
+        // ASSERT Constraint-2 : args0 != 0. (Internal Constraint)
+        // ASSERT Constraint-3 : Gas Limit <= Max Gas Block Limit.
+        // ASSERT Constraint-4 : Gas Limit >= Threshold.
+        
+        // COMMENT : Sample Gas Output Pair.
+        // Gas Price is 20000000000 wei
+        // gas estimation = 5071439 units
+        // gas cost estimation = 101428780000000000 wei
+        // gas cost estimation = 0.10142878 ether
+        const satPair = {
+            inputs : [100, 500],
+            gasPrice : "20000000000 wei",
+            gas_threshold : 5071439, 
+            value: 250
+        }
+        
+        // Gas Price is 20000000000 wei
+        // gas estimation = 8100239 units
+        // gas cost estimation = 162004780000000000 wei
+        // gas cost estimation = 0.16200478 ether
+        // ASSERT Fail : 8100239 > Max Gas Block Limit (6721975)
+        const unsatPair = {
+            inputs : [100, 800],
+            gasPrice : "20000000000 wei",
+            gas_threshold : 8100239,
+            value: 400
+        }
+
         var result = await sampleInstance.transferToAddressStub(
-            100,
-            1,
+            satPair.inputs[0], // args0
+            satPair.inputs[1], // args1
             accounts[0],
             {
                 from: accounts[1], 
                 // Value can't be zero or empty for this 
                 // call to pass through.
-                gas: 1000000,
-                value: web3.utils.toWei('1', 'ether')
+                gas: satPair.gas_threshold,
+                value: web3.utils.toWei(satPair.value.toString(), 'ether') // msg.value
             }
         );
         
-        console.log(result)
+        // console.log(result)
         
-        var result = await sampleInstance.getBalance();
+        // var result = await sampleInstance.depositContract({
+        //     from: accounts[0], value: web3.utils.toWei('57', 'ether')
+        // });
+        
+        console.log(result);
+        
+        var result = await sampleInstance.getContractBalance();
         console.log(result.toString())
                 
-        var result = await sampleInstance.getContractBalance();
+        var result = await sampleInstance.getOwnerBalance();
         console.log(result.toString())
                 
         var result = await sampleInstance.getBalanceSender();
@@ -72,7 +157,35 @@ let Main =  async () => {
         
     }
     
+    // COMMENT :
     TestHook().then(a => console.log("Done With Calls."));
 }
 
 module.exports = Main;
+
+// {
+//       logIndex: 98,
+//       transactionIndex: 0,
+//       transactionHash: '0x252f0cfd9a981945cbff28379b884d2ff14d4b936420c5468be72db831e0f4dd',
+//       blockHash: '0xd65c437c853246b345c35472866554325785c8c68a373980dc50d5a6d4290465',
+//       blockNumber: 54,
+//       address: '0x52f736E381a9d7234AC26f71Cfb8f1892FAE7d91',
+//       type: 'mined',
+//       id: 'log_483e9bcc',
+//       event: 'Looper',
+//       args: [Result]
+//     },
+//     {
+//       logIndex: 99,
+//       transactionIndex: 0,
+//       transactionHash: '0x252f0cfd9a981945cbff28379b884d2ff14d4b936420c5468be72db831e0f4dd',
+//       blockHash: '0xd65c437c853246b345c35472866554325785c8c68a373980dc50d5a6d4290465',
+//       blockNumber: 54,
+//       address: '0x52f736E381a9d7234AC26f71Cfb8f1892FAE7d91',
+//       type: 'mined',
+//       id: 'log_980560e9',
+//       event: 'Looper',
+//       args: [Result]
+//     },
+//     ... 400 more items
+// }
